f5c610cb5bebc02c94aed3dfd4ff6f14b2f82fcc
Patrick Rohr <patrickrohr92@gmail.com>
2015-08-03 08:43:51 -0500

    Adds Left Join Games to Matchups
    Also adds results controller (empty)

--- app/models/game.rb
+++ app/models/game.rb
@@ -9,4 +9,8 @@ class Game < ActiveRecord::Base
   def team_2
     @team_2 ||= Team.find(team_2_id)
   end
+
+  def self.join_left_gamedays
+    self.joins('LEFT JOIN gamedays ON games.gameday_id = gamedays.id').order('gamedays.date')
+  end
 end
