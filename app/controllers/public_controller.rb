83f0cf8a76f6340bf402f39a1f52d9d512b2dbf6
patrickrohr <patrickrohr92@gmail.com>
2016-09-01 14:00:10 -0500

    Allow from iframe

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,7 @@
 class PublicController < ApplicationController
   skip_authorization_check
+
+  response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM http://gltv.ch" # allow from IFRAME
   
   layout 'public'
   
